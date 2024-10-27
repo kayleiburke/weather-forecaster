843813bf84cedaffc26de3ea78dac80dcecbcc9b
Kaylei Burke <kayleiburke@nypl.org>
2024-10-27 15:45:48 -0500

    Update rspec tests to account for new forecast service

--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -3,22 +3,26 @@ require 'rails_helper'
 RSpec.describe "Forecasts", type: :request do
   describe "POST /forecast" do
 
-    it "retrieves forecast data for a valid address" do
-      # Mock the GeocodioClient singleton response
-      geocode_stub = load_json_stub('geocode_stub.json')
-      allow(GeocodioClient.instance).to receive(:geocode).and_return(geocode_stub)
+    let(:geocode_stub) do
+      load_json_stub('geocode_stub.json')
+    end
 
-      # Mock the OpenWeatherClient singleton response
-      weather_stub = {
+    let(:weather_stub) do
+      {
         'main' => { 'temp' => 75 },
         'weather' => [{ 'description' => 'clear sky', 'icon' => '01d' }]
       }
-      allow(OpenWeatherClient.instance).to receive(:current_weather).and_return(weather_stub)
+    end
 
-      # Perform the request
+    before do
+      # Mock the singleton responses
+      allow(GeocodioClient.instance).to receive(:geocode).and_return(geocode_stub)
+      allow(OpenWeatherClient.instance).to receive(:current_weather).and_return(weather_stub)
+    end
+
+    it "retrieves forecast data for a valid address" do
       post '/forecast', params: { address: 'One Apple Park Way, Cupertino, CA' }
 
-      # Verify the response and forecast data
       expect(response).to have_http_status(:ok)
 
       forecast = assigns(:forecast)
@@ -29,14 +33,10 @@ RSpec.describe "Forecasts", type: :request do
     end
 
     it "returns an error for an invalid address" do
-      # Mock the GeocodioClient singleton response
-      geocode_stub = { 'error' => 'Invalid address' }
-      allow(GeocodioClient.instance).to receive(:geocode).and_return(geocode_stub)
+      allow(GeocodioClient.instance).to receive(:geocode).and_return({ 'error' => 'Invalid address' })
 
-      # Perform the request
       post '/forecast', params: { address: 'Invalid Address' }
 
-      # Verify the response and error message
       expect(response).to have_http_status(:ok)
 
       error = assigns(:error)
@@ -44,13 +44,11 @@ RSpec.describe "Forecasts", type: :request do
     end
 
     it "caches forecast details for 30 minutes" do
-      # Enable caching and mock the cache fetch method
+      # Enable caching and mock the cache behavior
       allow(Rails.cache).to receive(:fetch).and_call_original
 
-      # Perform the request
       post '/forecast', params: { address: 'One Apple Park Way, Cupertino, CA 95014' }
 
-      # Verify that the forecast was cached according to its zip code
       expect(Rails.cache).to have_received(:fetch).with('95014_forecast', expires_in: 30.minutes)
     end
 
