9fe7c48b4a941cc2a586485a35a633bfa501429c
Kaylei Burke <kayleiburke@nypl.org>
2024-10-27 14:34:50 -0500

    Extract forecast logic into ForecastService

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,36 +5,14 @@ class ForecastsController < ApplicationController
 
   def create
     address = params[:address]
-    geocode_result = GeocodioClient.instance.geocode([address])
+    result = ForecastService.new(address).call
 
-    if geocode_result['error']
-      @error = 'Invalid address. Please try again.'
+    if result[:error]
+      @error = result[:error]
       render :show and return
     end
 
-    zip = geocode_result['results'][0]['address_components']['zip']
-    lat = geocode_result['results'][0]['location']['lat']
-    lng = geocode_result['results'][0]['location']['lng']
-
-    if !zip
-      geocode_result = GeocodioClient.instance.reverse(["#{lat},#{lng}"], [], 1)
-      zip = geocode_result['results'][0]['address_components']['zip']
-    end
-
-    from_cache = true
-    forecast = Rails.cache.fetch("#{zip}_forecast", expires_in: 30.minutes) do
-      from_cache = false
-      OpenWeatherClient.instance.current_weather(lat: lat, lon: lng, units: 'imperial')
-    end
-
-    @forecast = {
-      address: geocode_result['results'][0]['formatted_address'],
-      temperature: forecast['main']['temp'],
-      description: forecast['weather'][0]['description'],
-      icon: forecast['weather'][0]['icon'],
-      from_cache: from_cache
-    }
-
+    @forecast = result
     render :show
   end
 
---
+++ app/services/forecast_service.rb
@@ -0,0 +1,60 @@
+# app/services/forecast_service.rb
+class ForecastService
+  def initialize(address)
+    @address = address
+  end
+
+  def call
+    geocode_result = GeocodioClient.instance.geocode([@address])
+
+    # Handle invalid address
+    return { error: 'Invalid address. Please try again.' } if geocode_result['error']
+
+    zip, lat, lng = extract_location_data(geocode_result)
+
+    # Fetch from cache or make a new API call
+    forecast, from_cache = fetch_forecast(zip, lat, lng)
+
+    # Build the forecast response
+    build_forecast_response(geocode_result, forecast, from_cache)
+  end
+
+  private
+
+  def extract_location_data(geocode_result)
+    result = geocode_result['results'][0]
+    zip = result['address_components']['zip']
+    lat = result['location']['lat']
+    lng = result['location']['lng']
+
+    # the zip may be null if the address the user entered was very broad (ex. California),
+    # so we need to reverse geocode to get the zip
+    if !zip
+      reverse_result = GeocodioClient.instance.reverse(["#{lat},#{lng}"], [], 1)
+      zip = reverse_result['results'][0]['address_components']['zip']
+    end
+
+    [zip, lat, lng]
+  end
+
+  def fetch_forecast(zip, lat, lng)
+    from_cache = true
+    forecast = Rails.cache.fetch("#{zip}_forecast", expires_in: 30.minutes) do
+      from_cache = false
+      OpenWeatherClient.instance.current_weather(lat: lat, lon: lng, units: 'imperial')
+    end
+    [forecast, from_cache]
+  end
+
+  def build_forecast_response(geocode_result, forecast, from_cache)
+    result = geocode_result['results'][0]
+
+    {
+      address: result['formatted_address'],
+      temperature: forecast['main']['temp'],
+      description: forecast['weather'][0]['description'],
+      icon: forecast['weather'][0]['icon'],
+      from_cache: from_cache
+    }
+  end
+end
